2c77b54b2b09780c4e7fcef952e8d10eafd85b62
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2025-11-20 17:29:10 -0300

    feat: criando componente de botão e configurando fonte poppins

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+    label: string;
+    click?: () => void
+}>();
+</script>
+
+<template>
+    <button class="poppins-medium">{{ label }}</button>
+</template>
+<style scoped>
+button {
+    width: 100%;
+    height: 45px;
+
+    background-color: var(--primary-color);
+    color: var(--font-button-primary);
+    border-radius: 16px;
+    border: none;
+    transition: 0.5s;
+}
+
+button:hover {
+    transition: 0.5s;
+    cursor: pointer;
+    background-color: var(--secondary-color);
+}
+</style>
+
